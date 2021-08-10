850897d7b64ebd7181362c71c6976585b88acd16
Petya29 <petya29avramenko@gmail.com>
2021-08-10 23:20:16 +0300

    create data folder and file if doesn't exist + chat_id in data

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -3,34 +3,39 @@
     <div class="auth" v-if="!$store.state.chat_id">
       <h4>
         Please join to our Telegram bot, who be sent your trade messages to you.
-        <br />
-        <br />
-        <a href="https://t.me/POEnot_bot" target="_blank"
-          >https://t.me/POEnot_bot</a
+        <span class="bot-link" @click="openBrowser('https://t.me/POEnot_bot')"
+          >https://t.me/POEnot_bot</span
         >
-        <br />
-        <br />
         Enter your chat id
       </h4>
       <div class="row chat-id">
-        <div class="input-field col s12">
+        <div
+          :class="[
+            'input-field',
+            'col s12',
+            { 'input-field-error': validateError },
+          ]"
+        >
           <input
             id="password"
             type="password"
             class="validate"
             v-model="newChatId"
           />
+          <small class="chat_id-danger" v-if="validateError"
+            >chat id cannot be empty</small
+          >
           <label for="password">chat id</label>
         </div>
       </div>
-      <a class="waves-effect waves-light btn">Save</a>
+      <a class="waves-effect waves-light btn" @click="storeChatId">Save</a>
     </div>
     <div class="main"></div>
     <scale-loader
       :loading="loading"
-      :color="color"
-      :height="height"
-      :width="width"
+      :color="'#ffffff'"
+      :height="'200px'"
+      :width="'200px'"
     ></scale-loader>
   </div>
 </template>
@@ -38,6 +43,7 @@
 <script>
 import fs from "fs";
 import { ref } from "vue";
+import { shell, remote } from "electron";
 import axios from "axios";
 import M from "materialize-css";
 import readLastLines from "read-last-lines";
@@ -46,22 +52,70 @@ import ScaleLoader from "vue-spinner/src/ScaleLoader.vue";
 export default {
   name: "HelloWorld",
   setup() {
+    const validateError = ref(false);
     const loading = ref(false);
+    const newChatId = ref("");
     const chatId = "123";
     const url = `${process.env.VUE_APP_TELEGRAM_API}sendMessage?chat_id=${chatId}&text=`;
     const LogFile = process.env.VUE_APP_LOG_PATH;
-    console.log(LogFile);
-    const clientLogs = ref([]);
-    fs.watchFile(LogFile, { interval: 100 }, () => {
+    fs.watchFile(LogFile, { interval: 500 }, () => {
       readLastLines.read(LogFile, 1).then((lines) => {
         axios.post((url + lines).trim());
         console.log(typeof lines);
       });
     });
 
+    function openBrowser(url) {
+      shell.openExternal(url);
+    }
+
+    function storeChatId() {
+      if (newChatId.value != "") {
+        const storagePath = `${remote.app.getPath(
+          "home"
+        )}\\AppData\\Local\\${remote.app.getName()}`;
+        if (!fs.existsSync(storagePath)) {
+          fs.mkdirSync(storagePath);
+        }
+        if (!fs.existsSync(`${storagePath}\\data.json`)) {
+          const dataJSON = {
+            chat_id: newChatId.value,
+          };
+          fs.writeFileSync(
+            `${storagePath}\\data.json`,
+            JSON.stringify(dataJSON)
+          );
+        } else {
+          if (
+            Object.keys(fs.readFileSync(`${storagePath}\\data.json`)).length > 0
+          ) {
+            const dataJSON = JSON.parse(
+              fs.readFileSync(`${storagePath}\\data.json`)
+            );
+            if (
+              dataJSON["chat_id"] !== undefined ||
+              dataJSON["chat_id"] !== ""
+            ) {
+              dataJSON["chat_id"] = newChatId.value;
+              fs.writeFileSync(
+                `${storagePath}\\data.json`,
+                JSON.stringify(dataJSON)
+              );
+            }
+          }
+        }
+        validateError.value = false;
+      } else {
+        validateError.value = true;
+      }
+    }
+
     return {
+      validateError,
       loading,
-      clientLogs,
+      newChatId,
+      openBrowser,
+      storeChatId,
     };
   },
   components: { ScaleLoader },
@@ -80,5 +134,23 @@ export default {
 }
 .validate {
   color: #ffffff !important;
+}
+.bot-link {
+  display: block;
+  cursor: pointer;
+  margin: 20px 0;
+  color: #1f1fe9 !important;
+}
+.chat_id-danger {
+  margin-top: -5px;
+  color: rgb(238, 75, 75) !important;
+  float: left;
+}
+.input-field-error input {
+  border-color: rgb(238, 75, 75) !important;
+  box-shadow: 0 1px 0 0 rgb(238, 75, 75) !important;
+}
+.input-field-error label {
+  color: rgb(238, 75, 75) !important;
 }
 </style>
